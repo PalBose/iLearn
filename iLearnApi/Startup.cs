9595a566327c8bbb0596472695686dbf0a1a325e
Pallavi Kamble <Pallavi_Kamble@Dell.com>
2020-08-08 17:57:06 +0530

    build error

--- iLearnApi/Startup.cs
+++ iLearnApi/Startup.cs
@@ -26,7 +26,7 @@ namespace iLearnApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<ITeacherRepository, TeacherRepository>();
+            services.AddSingleton<ITeacherRepository, MockTeacherRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
