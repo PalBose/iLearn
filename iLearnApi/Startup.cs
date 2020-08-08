130f2fc497174e259d9446d9cc13743ca368f5de
Pallavi Kamble <Pallavi_Kamble@Dell.com>
2020-08-08 23:33:55 +0530

    Updated Startup

--- iLearnApi/Startup.cs
+++ iLearnApi/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iLearnApi.DbContexts;
 using iLearnApi.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,8 +28,9 @@ namespace iLearnApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(option=>option.UseSqlServer(Configuration.GetConnectionString("iLearnDb")));
             services.AddSingleton<ITeacherRepository, MockTeacherRepository>();
-            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
