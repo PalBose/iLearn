8e1fa1a2e533ea9a26aa3cd40479e856f5613dae
Pallavi Kamble <Pallavi_Kamble@Dell.com>
2020-08-08 23:13:36 +0530

    latest entityFramework

--- iLearnApi/Startup.cs
+++ iLearnApi/Startup.cs
@@ -27,7 +27,7 @@ namespace iLearnApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<ITeacherRepository, MockTeacherRepository>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
